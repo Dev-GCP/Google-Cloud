fc4e49b40ca42a9e68f93b28fc5e23a5d69ff608
Dev-gcp <123863626+Dev-GCP@users.noreply.github.com>
2025-07-14 12:03:17 +0530

    Create command.sh

---
+++ Creating and Alerting on Logs-based Metrics/command.sh
@@ -0,0 +1,26 @@
+
+### 💡 Lab Link: [Creating and Alerting on Logs-based Metrics - GSP091](https://www.cloudskillsboost.google/focuses/619?parent=catalog)
+
+
+---
+
+### ⚠️ Disclaimer
+- **This script and guide are provided for  the educational purposes to help you understand the lab services and boost your career. Before using the script, please open and review it to familiarize yourself with Google Cloud services. Ensure that you follow 'Qwiklabs' terms of service and YouTube’s community guidelines. The goal is to enhance your learning experience, not to bypass it.**
+
+#
+### 🚨Copy and run the below commands in Cloud Shell:
+
+
+```
+curl -LO raw.githubusercontent.com/Techcps/Google-Cloud-Skills-Boost/master/Creating%20and%20Alerting%20on%20Logs%20based%20Metrics/techcps091.sh
+sudo chmod +x techcps091.sh
+./techcps091.sh
+```
+---
+
+### Congratulations, you're all done with the lab 😄
+
+---
+
+
+
